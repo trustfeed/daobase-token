9facb22ed4e1de0845a200140e5cad6df46d5df5
wittgena <wittgena@gmail.com>
2018-09-13 20:39:52 +0900

    Change migrate Contract to DaobaseTokenFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,29 @@
-const DaobaseToken = artifacts.require('./DaobaseToken.sol');
+const DaobaseTokenFactory = artifacts.require('./DaobaseTokenFactory.sol');
 
-module.exports = deployer =>
-  deployer.deploy(DaobaseToken);
+module.exports = function (deployer, network, accounts) {
+  const openingTime = Date.now() / 1000;
+  const closingTime = openingTime + 100 * 60 * 60 * 24;
+  const rate = 10000;
+  const hardCap = 25000;
+  const softCap = 5000;
+  const name = 'DaobaseToken';
+  const symbol = 'DBT';
+  const decimals = 18;
+  const init = 10000000000;
+   return deployer
+    .then(() => {
+      return deployer
+    .deploy(
+      DaobaseTokenFactory,
+      [accounts[0], accounts[1]],
+      name,
+      symbol,
+      decimals,
+      init,
+      openingTime,
+      closingTime,
+      rate,
+      hardCap,
+      softCap);
+  });
+}
